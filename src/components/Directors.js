575ca9c1d1c0eeacdeb5bc8bdbd750c4da15b441
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-09-27 16:06:59 +0300

    Completed assignmnet

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,14 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  
+  
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorsList}
+    </div>
+  );
 }
 
 export default Directors;
