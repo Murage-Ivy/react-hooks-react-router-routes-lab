b8e70803f1d21bafcfcff7e60cb22a9af37e273e
Murage-Ivy <ivy.murage@student.moringaschool.com>
2022-09-27 16:31:12 +0300

    Update the Directors  compnenet

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -3,6 +3,16 @@ import { directors } from "../data";
 
 function Directors() {
   
+  const directorsList = directors.map((director, index) => (
+    <div key={index}>
+      {director.name}
+      <ul>
+        {director.movies.map((movie, index) => (
+          <li key={index}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
   
   return (
     <div>
